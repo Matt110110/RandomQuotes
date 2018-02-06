1432ec79dbe841999e36434d9d2284aa4a8b66d2
Matt110110 <matruprasadchand@gmail.com>
2018-02-06 09:28:18 +0530

    My first commit

---
+++ app/src/main/java/io/github/matt110110/randomquotes/IntroActivity.java
@@ -0,0 +1,13 @@
+package io.github.matt110110.randomquotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
